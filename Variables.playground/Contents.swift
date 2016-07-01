d10e7ceb094804edeb92651f7a8ec74db16bcd76
James Birchall <jamesbirchall@gmail.com>
2016-07-01 21:24:36 +0100

    Chapter 2 code
    Chapter 2 code

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+//var numberOfStopLights = "Four"
+//var numberOfStopLights: Int = 4
+let numberOfStopLights: Int = 4
+//numberOfStopLights += 2
+
+var population: Int
+population = 5422
+
+let townName: String = "Jimtown"
+
+var townUnemploymentLevel = 25
+
+let townDescription = "\(townName) has a population of \(population), of which \(townUnemploymentLevel) are unemployed and has \(numberOfStopLights) stoplights"
+
+print(townDescription)
+
