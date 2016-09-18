0463b8831894ba81198689b870ac1a235694b232
James Birchall <jamesbirchall@gmail.com>
2016-09-18 09:52:14 +0100

    Chapter 15 completed
    subclassing exercise, including using static variable accessed as a
    class variable to persist contents.

--- MonsterTown/MonsterTown/main.swift
+++ MonsterTown/MonsterTown/main.swift
@@ -9,16 +9,33 @@
 import Foundation
 
 var myTown = Town() // struct gets init here - a default one
-myTown.changePopulation(amount: -1)
-myTown.townName = "Somewheresville"
+//myTown.changePopulation(amount: 10)
+myTown.townName = "Spooksville"
 //myTown.printDescription()
 //
 //let gm = Monster()
 //gm.town = myTown
 //gm.terrorizeTown()
+//
+//let fredTheZombie = Zombie()
+//fredTheZombie.town = myTown
+//fredTheZombie.changeName(name: "Fred", walksWithLimp: true)
+//fredTheZombie.terrorizeTown()
+//fredTheZombie.town?.printDescription()  // optional chaining again
 
-let fredTheZombie = Zombie()
-fredTheZombie.town = myTown
-fredTheZombie.changeName(name: "Fred", walksWithLimp: true)
-fredTheZombie.terrorizeTown()
-fredTheZombie.town?.printDescription()  // optional chaining again
+let masterVampire = Vampire()
+masterVampire.name = "Master Vampire"
+masterVampire.town = myTown
+
+masterVampire.terrorizeTown()
+masterVampire.terrorizeTown()
+masterVampire.terrorizeTown()
+masterVampire.terrorizeTown()
+masterVampire.terrorizeTown()
+masterVampire.town?.printDescription()
+
+// prove that the static vampire horde is global to the Vampire class
+for vamp in Vampire.vampireHorde {
+    print("\(vamp.name)")
+}
+
