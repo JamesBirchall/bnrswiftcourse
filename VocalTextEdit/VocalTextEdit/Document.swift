2dd73819b4ccf49b90724f2b26fd3051cc93911d
James Birchall <jamesbirchall@gmail.com>
2016-09-29 20:43:07 +0100

    App chapter ready
    Code for app works, save and load function, speak and stop speaking
    works

--- VocalTextEdit/VocalTextEdit/Document.swift
+++ VocalTextEdit/VocalTextEdit/Document.swift
@@ -9,11 +9,8 @@
 import Cocoa
 
 class Document: NSDocument {
-
-    override init() {
-        super.init()
-        // Add your subclass-specific initialization here.
-    }
+    
+    var contents: String = " "
 
     override class func autosavesInPlace() -> Bool {
         return true
@@ -23,20 +20,40 @@ class Document: NSDocument {
         // Returns the Storyboard that contains your Document window.
         let storyboard = NSStoryboard(name: "Main", bundle: nil)
         let windowController = storyboard.instantiateController(withIdentifier: "Document Window Controller") as! NSWindowController
+        
+        let viewController = windowController.contentViewController as! ViewController
+        viewController.contents = contents
+        
         self.addWindowController(windowController)
     }
 
     override func data(ofType typeName: String) throws -> Data {
-        // Insert code here to write your document to data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning nil.
-        // You can also choose to override fileWrapperOfType:error:, writeToURL:ofType:error:, or writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        
+        // to save data out from a document
+        let windowController = windowControllers[0]
+        let viewController = windowController.contentViewController as! ViewController
+        
+        let contents = viewController.contents ?? "" // take string or empty string if nil
+        
+        if let data = contents.data(using: String.Encoding.utf8) {
+            return data
+        } else {
+            let userInfo = [NSLocalizedRecoverySuggestionErrorKey: "File cannot be encoded in UTF-8"]
+            throw NSError(domain: "com.birchall.bnrswift.VocalTextEdit", code: 0, userInfo: userInfo)
+        }
     }
 
     override func read(from data: Data, ofType typeName: String) throws {
-        // Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
-        // You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
-        // If you override either of these, you should also override -isEntireFileLoaded to return false if the contents are lazily loaded.
-        throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
+        
+        let contents: String? = String(bytes: data, encoding: .utf8)
+        
+        if contents != nil {
+            Swift.print(contents)
+            self.contents = contents!   // we are safe at this point so unwrap
+        } else {
+            let userInfo = [NSLocalizedRecoverySuggestionErrorKey: "File is not valid UTF-8"]
+            throw NSError(domain: "com.birchall.bnrswift.VocalTextEdit", code: 0, userInfo: userInfo)
+        }
     }
 
 
--- VocalTextEdit/VocalTextEdit/ViewController.swift
+++ VocalTextEdit/VocalTextEdit/ViewController.swift
@@ -9,15 +9,33 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    let speechSynthesiser = NSSpeechSynthesizer()
 
     @IBOutlet var textView: NSTextView!
     
+    var contents: String? {
+        get {
+            return textView.string
+        }
+        
+        set {
+            textView.string = newValue
+        }
+    }
+    
     @IBAction func speakButtonClicked(sender: NSButton) {
-        print("I should speak \(textView.string)")
+        if let contents = textView.string , contents != "" {
+            speechSynthesiser.startSpeaking(contents)
+        } else {
+            speechSynthesiser.startSpeaking("Please enter some words for me to speak.")
+        }
     }
     
     @IBAction func stopButtonClicked(sender: NSButton) {
-        print("Stop button was clicked")
+        if speechSynthesiser.isSpeaking {
+            speechSynthesiser.stopSpeaking()
+        }
     }
 }
 
